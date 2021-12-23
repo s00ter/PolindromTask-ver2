7c3e1d04f0b4de784874993f9e28702cd2ede3b2
User <User@DESKTOP-8SJOJLK>
2021-12-23 13:58:28 +0300

    Добавьте файлы проекта.

---
+++ PolindromTask-ver2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace PolindromTask_ver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? str;
+            Console.WriteLine("Введите строку");
+            str = Console.ReadLine();
+            str = str.ToLower().Replace(" ", string.Empty);
+
+            string str1 = new string(str.ToCharArray().Reverse().ToArray());
+            if (str == str1)
+            {
+                Console.WriteLine("Введённая строка является полиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Введённая строка не является полиндромом");
+            }
+            
+        }
+    }
+}
